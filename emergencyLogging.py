85680eade18e19ab61e8188c1720979b237873a3
Tyler Bramlett <49885100+AngriBroccoli@users.noreply.github.com>
2019-04-25 09:26:54 -0600

    Create emergencyLogging.py

---
+++ emergencyLogging.py
@@ -0,0 +1,45 @@
+'''
+CS 3030
+Emergency Distress Program
+Tyler Bramlett
+This file will log, and save emergency data to a .txt file.
+'''
+
+import os
+import time
+import datetime
+
+
+def readFromFile(file):
+    os.getcwd()
+    logFile = open(file)
+    print(f"\n\nREADING CONTENTS OF FILE: {file}\n\n" + logFile.read())
+    time.sleep(1)
+    print(logFile.read())
+    logFile.close()
+
+
+def addToFile(data, file):
+    os.getcwd()
+    logFile = open(file, "a")
+    logFile.write(data + "\n")
+    logFile.write(str(datetime.datetime.now()) + "\n")
+    logFile.close()
+
+
+# Global vars to use across files:
+
+currentAddress = "F.E.M.A. Station Colorado Springs"
+currentStatus = "Safe, Battery Low, Uninjured"
+
+# File names to use:
+fileName001 = "EmergencyLog.txt"
+fileName002 = "EmergencyInfo.txt"
+
+# Add specific data to file:
+addToFile("\nLOCATION: " + currentAddress, fileName001)
+addToFile("STATUS: " + currentStatus, fileName001)
+
+# Read data from these files
+readFromFile(fileName001)
+readFromFile(fileName002)
