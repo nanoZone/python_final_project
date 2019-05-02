bc8d9575a1d7e226a4dd31de5ef1d0431ff99934
Tyler Bramlett <49885100+AngriBroccoli@users.noreply.github.com>
2019-05-02 09:33:52 -0600

    Update importContactsandMessage.py

--- TylersCode/importContactsandMessage.py
+++ TylersCode/importContactsandMessage.py
@@ -1,1 +1,10 @@
 
+def getPreviousLocation(file):
+    locationList = []
+    with open(file, 'rt') as myfile:    # Open file lorem.txt for reading text
+        for line in myfile:             # For each line, read it to a string
+            if "LOCATION: " in line:
+                locationList.append(line.replace("LOCATION:", ''))
+                # print(line.replace("LOCATION:", ''))
+return locationList
+
