8176bdcb9ca0f308e7967d82f4ae326f5592b6b1
Tyler Bramlett <49885100+AngriBroccoli@users.noreply.github.com>
2019-05-07 15:48:13 -0600

    Update sendEmailSMS.py

--- TylersCode/sendEmailSMS.py
+++ TylersCode/sendEmailSMS.py
@@ -2,14 +2,29 @@
 # to multiple users  
 # from your Gmail account  
 import smtplib 
-  
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
 # list of email_id to send the mail 
 li = ["xxxxx@gmail.com", "yyyyy@gmail.com"] 
-  
-for i in range(len(li)): 
-    s = smtplib.SMTP('smtp.gmail.com', 587) 
-    s.starttls() 
-    s.login("sender_email_id", "sender_email_id_password") 
-    message = "Message_you_need_to_send"
-    s.sendmail("sender_email_id", li[i], message) 
-    s.quit() 
+
+email = 'xxxx@gmail.com'
+password = str(input("\nENTER PASSWORD >> "))
+send_to_email = 'xxxx@gmail.com'
+subject = "EMERGENCY DISTRESS"
+message = 'ALERT!!!'
+
+msg = MIMEMultipart()
+msg['From'] = email
+msg['To'] = send_to_email
+msg['Subject'] = subject
+
+# Attach the message to the MIMEMultipart object
+msg.attach(MIMEText(message, 'plain'))
+
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(email, password)
+text = msg.as_string()# You now need to convert the MIMEMultipart object to a string to send
+server.sendmail(email, send_to_email, text)
+server.quit()
