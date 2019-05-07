36738bb5c4acdf8d88ee9b53c5063102a0222cdc
Tyler Bramlett <49885100+AngriBroccoli@users.noreply.github.com>
2019-05-06 19:46:33 -0600

    Update importContactsandMessage.py

--- TylersCode/importContactsandMessage.py
+++ TylersCode/importContactsandMessage.py
@@ -11,26 +11,32 @@ import re
 
 
 def findContactInfoInFile(file):
+
     os.getcwd()
     logFile = open(file)
     string = logFile.read()
     emailList = re.findall('\S+@\S+', string)
     phoneNumRegex = re.compile(r"(\d\d\d)-(\d\d\d)-(\d\d\d\d)")
     phoneList = phoneNumRegex.findall(string)
+
     # Printing of List
     print("Email addresses within this file are: ", emailList)
     print("Phone numbers within this file are: ", phoneList)
+
     logFile.close()
 
 
 def regexPhoneNumbers():
+
     # input statement
     s = input("Please provide best phone number xxx-xxx-xxxx, email.")
+
     # \S matches any non-whitespace character
     # @ for as in the Email
     # + for Repeats a character one or more times
     lst = re.findall('\S+@\S+', s)
     lst1 = re.findall('\S[0-9]+\S+-\S[0-9]+', s)
+    
     # Printing of List
     print("email address: ", lst)
     print("phone numbers are: ", lst1)
