65a09746f1cc7cd8aad9cb50305a8819817a0267
Tyler Bramlett <49885100+AngriBroccoli@users.noreply.github.com>
2019-05-06 20:40:43 -0600

    Update main.py

--- TylersCode/main.py
+++ TylersCode/main.py
@@ -32,14 +32,46 @@ def exportAsPDF(message):
     pdf.output("simple_demo.pdf")
 
 
-exportAsPDF(readLinesFromFile("EmergencyLog.txt"))
+def findWeatherData():
+    # api key
+    api_key = "9d0dd491847df6d883ef71401d8af3ec"
+    # base url variable to store url
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+    # enter city name
+    city_name = input("Enter city name: ")
+    # complete_url variable to store complete url address
+    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+    # return respone object and get method
+    response = requests.get(complete_url)
+    # json method of response object to convert json format data to python
+    x = response.json()
 
-
-
-
-
-
-
+    print("\nPrinting Weather Data: " + str(x))
+    # check the value of "cod" key is equal to "404", means city is
+    # found, otherwise city is not found
+    if x["cod"] != "404":
+        # store value of main key in variable y
+        y = x["main"]
+        # store the value corresponding to "temp" key of y
+        current_temperature = y["temp"]
+        # store the value corresponding to the "pressure" key of y
+        current_pressure = y["pressure"]
+        # store the value corresponding to the "humidity" key of y
+        current_humidity = y["humidity"]
+        # store the value of "weather" key in variable z
+        z = x["weather"]
+        # store the value corresponding to "description" key at 0 index of z
+        weather_description = z[0]["description"]
+        print("\n\nTemperature (in kelvin unit) = " +
+              str(current_temperature) +
+              "\nAtmospheric pressure (in hPa unit) = " +
+              str(current_pressure) +
+              "\nHumidity (in percentage) = " +
+              str(current_humidity) +
+              "\nDescription = " +
+              str(weather_description))
+    else:
+        print(" City Not Found ")
 
 
 def reportLocation(address, status):
@@ -71,18 +103,6 @@ def getPreviousLocation(file):
     return locationList
 
 
-# Global vars across files:
-currentStatus = "Safe, Battery Low, Uninjured"
-
-createLocationMap(getPreviousLocation("EmergencyLog.txt"))
-
-
-
-
-
-
-
-
 def readContentsFromFile(file):
     os.getcwd()
     logFile = open(file)
@@ -98,28 +118,6 @@ def addToFile(data, file):
     logFile.write(data + "\n")
     logFile.write(str(datetime.datetime.now()) + "\n")
     logFile.close()
-
-
-# Global vars to use across files:
-currentAddress = "F.E.M.A. Station Colorado Springs"
-currentStatus = "Safe, Battery Low, Uninjured"
-
-# File names to use:
-fileName001 = "EmergencyLog.txt"
-fileName002 = "EmergencyInfo.txt"
-
-# Add specific data to file:
-addToFile("\nLOCATION: " + currentAddress, fileName001)
-addToFile("STATUS: " + currentStatus, fileName001)
-
-# Read data from these files
-readContentsFromFile(fileName001)
-readContentsFromFile(fileName002)
-
-
-
-
-
 
 
 def findContactInfoInFile(file):
@@ -156,42 +154,24 @@ def regexPhoneNumbers():
 
 findContactInfoInFile("EmergencyInfo.txt")
 
-# api key
-api_key = "9d0dd491847df6d883ef71401d8af3ec"
-# base url variable to store url
-base_url = "http://api.openweathermap.org/data/2.5/weather?"
-# enter city name
-city_name = input("Enter city name: ")
-# complete_url variable to store complete url address
-complete_url = base_url + "appid=" + api_key + "&q=" + city_name
-# return respone object and get method
-response = requests.get(complete_url)
-# json method of response object to convert json format data to python
-x = response.json()
+createLocationMap(getPreviousLocation("EmergencyLog.txt"))
 
-print("\nPrinting Weather Data: " + str(x))
-# check the value of "cod" key is equal to "404", means city is
-# found, otherwise city is not found
-if x["cod"] != "404":
-    # store value of main key in variable y
-    y = x["main"]
-    # store the value corresponding to "temp" key of y
-    current_temperature = y["temp"]
-    # store the value corresponding to the "pressure" key of y
-    current_pressure = y["pressure"]
-    # store the value corresponding to the "humidity" key of y
-    current_humidity = y["humidity"]
-    # store the value of "weather" key in variable z
-    z = x["weather"]
-    # store the value corresponding to "description" key at 0 index of z
-    weather_description = z[0]["description"]
-    print("\n\nTemperature (in kelvin unit) = " +
-          str(current_temperature) +
-          "\nAtmospheric pressure (in hPa unit) = " +
-          str(current_pressure) +
-          "\nHumidity (in percentage) = " +
-          str(current_humidity) +
-          "\nDescription = " +
-          str(weather_description))
-else:
-    print(" City Not Found ")
+
+exportAsPDF(readLinesFromFile("EmergencyLog.txt"))
+
+
+# Global vars to use across files:
+currentAddress = "F.E.M.A. Station Colorado Springs"
+currentStatus = "Safe, Battery Low, Uninjured"
+
+# File names to use:
+fileName001 = "EmergencyLog.txt"
+fileName002 = "EmergencyInfo.txt"
+
+# Add specific data to file:
+addToFile("\nLOCATION: " + currentAddress, fileName001)
+addToFile("STATUS: " + currentStatus, fileName001)
+
+# Read data from these files
+readContentsFromFile(fileName001)
+readContentsFromFile(fileName002)
